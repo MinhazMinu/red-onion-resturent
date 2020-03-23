5566c2508c681da6cb15637d62c70e9e53c59a7b
minhaz <minhaz.taher@gmail.com>
2020-03-23 15:58:10 +0600

    reset everything and done again

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "./App.css";
+import Header from "./Components/Header/Header";
+import Banner from "./Components/Banner/Banner";
+import Foods from "./Components/Foods/Foods";
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  BrowserRouter
+} from "react-router-dom";
+import Footer from "./Components/Footer/Footer";
+
+import FoodDetails from "./Components/FoodDetails/FoodDetails";
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="main">
+        <Header></Header>
+
+        {/* Switch Able  component */}
+        <Switch>
+          <Route exact path="/">
+            <Banner></Banner>
+            <Foods></Foods>
+          </Route>
+          <Route path="/food/:id">
+            <FoodDetails></FoodDetails>
+          </Route>
+          <Route path="/login">
+            <h1>Welcome to Login</h1>
+          </Route>
+        </Switch>
+        {/* End of Switch Able  component */}
+        <Footer></Footer>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
